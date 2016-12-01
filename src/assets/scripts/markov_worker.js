a2929a3520b98f82fbd97efc74cf542dc4a22ec3
Michelle Steigerwalt <msteigerwalt@gmail.com>
2016-12-01 13:11:47 -0500

    Workers: Documentation and reliability tweaks.

--- src/assets/scripts/markov_worker.js
+++ src/assets/scripts/markov_worker.js
@@ -1,16 +1,26 @@
 // Web Worker for Markov.
 
-var prefix = "MRKV_";  // Incoming command prefix.
-var namespace = "";    // localStorage key namespace.
+let prefix = ""; // Incoming command prefix.
+var namespace = ""; // localStorage key namespace.
 
 var loop = false;
 onmessage = function(e) {
-    var pre = new RegExp('^'+prefix);
-    if (!e.data.match(pre)) return;
-    var data = JSON.parse(e.data.replace(pre, ''));
-    var [command, args, id] = data;
-    loop = loop || new EventLoop();
-    loop.add(command, args, id);
+    // We're expecting the data packet to be an array we can
+    // pop from.
+    if (!e.data || !e.data.splice) {
+        return;
+    }
+    var pre = e.data[0];
+    // Is this a command to set the prefix?
+    if (pre == "__WKRPREFIX") {
+        prefix = e.data[1];
+    // Does it have the prefix we're expecting?
+    } else if (pre == prefix) {
+        // Then make an event loop and send it to the handler.
+        var [id, command, args] = e.data[1];
+        loop = loop || new EventLoop();
+        loop.add(id, command, args);
+    }
 };
 
 // These things need to return an estimate of the number of iterations,
@@ -47,23 +57,38 @@ class EventLoop {
         this._interval = interval;
     }
 
+    /**
+     * Run a command and add any returned generator to the generators
+     * array so we can loop through it on every step until it's depleted.
+     *
+     * id is the handler ID that comes in from the window process. 
+     */
     add(id, cmd, args) {
-        if (!API[cmd]) {
-            console.error("Invalid command:", cmd);
+        if (!API[cmd]) { // Womp womp.
+            console.error(prefix, "Invalid command:", cmd);
             return;
         }
+        // Run the command.
         var result = API[cmd].apply(null, args);
+        // If we get a generator, add that to our generators so we can
+        // step through it.
+        //
+        // We're expecting a generator command to return an array
+        // containing [generator, total].
         if (result[0] && result[0].next) {
             let [gen, total] = result;
             let counter = {current:0, total:total};
-            postMessage([id, 'start', [null, total]]);
+            postMessage([id, 'start', [total]]);
             this._generators.set(id, gen);
             this._counts.set(id, counter);
             this._run_loop();
+        // If we didn't get a generator, just send the result.
         } else postMessage([id, 'done', [result]]);
     }
 
     _run_loop() {
+        // Loop through all the generators and send a progress update to
+        // the handler ID.
         for (let [id, gen] of this._generators.entries()) {
             let {value, done} = gen.next();
             value = value || null;
@@ -75,9 +100,12 @@ class EventLoop {
                 this._counts.delete(id);
             } else c.current++;
         }
+        // If we have at least one active generator, set an interval so
+        // this will be called again.
         if (this._generators.size > 0) {
             this._active = true;
             setTimeout(()=>{ this._run_loop(); }, this._interval);
+        // Otherwise, just chill until we get some other command.
         } else {
             this._active = false;
         }
--- src/assets/scripts/worker_api.js
+++ src/assets/scripts/worker_api.js
@@ -1,12 +1,55 @@
+/**
+ * Simple interface for initializing Web Workers, running commands on
+ * them, and attaching events to the commands.
+ *
+ *     var api = WorkerAPI();
+ *     var req = api.request('count', 5);
+ *     req.on('progress', function(result, index, total) {
+ *         console.log("got result", result);
+ *         console.log("processed", index, "of", total);
+ *         console.log(Math.floor(index/total*100)+"% complete");
+ *     });
+ */
 class WorkerAPI {
 
-    constructor(workerName, cmdPrefix="") {
-        this._prefix = cmdPrefix;
-        this._worker = new Worker("/scripts/"+workerName+'.js');
+    /**
+     * Load a webworker and get a simple request interface that keeps
+     * track of execution progress via generators in the webworker.
+     */
+    constructor(name, cmdPrefix="") {
+        // Send the command prefix to the worker.
+        this._prefix = cmdPrefix || name.toUpperCase();
+        this._worker = new Worker("/scripts/"+name+'.js');
+        this._worker.postMessage(['__WKRPREFIX', this._prefix]);
+        // Set up event handler.
         this._worker.onmessage = (e) => this._handleMessage(e);
         this._reqs = [];
     }
 
+    /**
+     * Create a new WorkerRequest.
+     *
+     * Requests emit the following events:
+     *
+     *   - start(total)
+     *
+     *        Posted if a generator is started by the command handler
+     *        for this request.
+     *
+     *   - progress(result, current, total)
+     *
+     *        Last yield value, the current cursor, and the total number
+     *        of items.
+     *
+     *   - done(result)
+     *
+     *        Fired after the generator finishes executing. Returns the
+     *        final yielded value.
+     *
+     * The request isn't sent immediately.  Set your callbacks and then
+     * call req.send().
+     *
+     */
     request(command, ...args) {
         var id = this._reqs.length;
         this._reqs.push(new WorkerRequest(id, this, [command, args]));
@@ -20,14 +63,21 @@ class WorkerAPI {
         this._reqs[id]._handleMessage.apply(this._reqs[id], [type, args]);
     }
 
-    send(worker_id, req="") {
+    /**
+     * Send a message to the web worker.
+     */
+    _postMessage(worker_id, req="") {
         var [command, args] = req;
-        var p = JSON.stringify([worker_id, command, args]);
-        this._worker.postMessage(this._prefix+p);
+        var p = [worker_id, command, args];
+        this._worker.postMessage([this._prefix, p]);
     }
 
 }
 
+/**
+ * A simple interface for setting event handlers and then starting
+ * a given request to a web worker.
+ */
 class WorkerRequest {
 
     constructor(id, api, req) {
@@ -38,7 +88,7 @@ class WorkerRequest {
     }
 
     start() {
-        this._api.send.apply(this._api, [this._id, this._req]);
+        this._api._postMessage.apply(this._api, [this._id, this._req]);
     }
 
     on(evt, fn) {
